c953b228cfbfe0535cb3b1252b145010e36140e1
unknown <bachuvaishnavi098@gmail.com>
2026-07-14 17:21:44 +0530

    Remove conflicting CORS credentials

--- backend/LoginProject/src/main/java/com/example/loginproject/controller/UserController.java
+++ backend/LoginProject/src/main/java/com/example/loginproject/controller/UserController.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
